f30dea632d6a39ae6f3d015f6514fc1c517c2523
Jab1k <jobir4166@gmail.com>
2022-11-07 19:44:20 +0500

    franc

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,36 @@ class MyApp extends StatelessWidget {
           child: Container(
               color: Colors.black,
               child: Center(
-                child: Column(
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      height: 100,
-                      width: 300,
+                      height: 500,
+                      width: 100,
                       decoration: const BoxDecoration(
-                          color: Colors.white,
+                          color: Colors.blue,
                           borderRadius: BorderRadius.only(
                               topLeft: Radius.circular(16),
-                              topRight: Radius.circular(16))),
+                              bottomLeft: Radius.circular(16))),
+                      child: Center(
+                        child: Text('F',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                decoration: TextDecoration.none,
+                                color: Colors.black)),
+                      ),
+                    ),
+                    // Container(
+                    //   height: 10,
+                    //   width: 500,
+                    //   decoration: const BoxDecoration(
+                    //       color: Colors.red,
+                    //       borderRadius: BorderRadius.all(Radius.circular(16))),
+                    // ),
+                    Container(
+                      width: 100,
+                      height: 500,
+                      decoration: const BoxDecoration(color: Colors.white),
                       child: Center(
                         child: Text('R',
                             textAlign: TextAlign.center,
@@ -42,34 +61,15 @@ class MyApp extends StatelessWidget {
                     //       borderRadius: BorderRadius.all(Radius.circular(16))),
                     // ),
                     Container(
-                      width: 300,
-                      height: 100,
-                      decoration: const BoxDecoration(color: Colors.blue),
-                      child: Center(
-                        child: Text('U',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                decoration: TextDecoration.none,
-                                color: Colors.black)),
-                      ),
-                    ),
-                    // Container(
-                    //   height: 10,
-                    //   width: 500,
-                    //   decoration: const BoxDecoration(
-                    //       color: Colors.red,
-                    //       borderRadius: BorderRadius.all(Radius.circular(16))),
-                    // ),
-                    Container(
-                      height: 100,
-                      width: 300,
+                      height: 500,
+                      width: 100,
                       decoration: const BoxDecoration(
                           color: Colors.red,
                           borderRadius: BorderRadius.only(
-                              bottomLeft: Radius.circular(16),
+                              topRight: Radius.circular(16),
                               bottomRight: Radius.circular(16))),
                       child: Center(
-                        child: Text('S',
+                        child: Text('A',
                             textAlign: TextAlign.center,
                             style: TextStyle(
                                 decoration: TextDecoration.none,
